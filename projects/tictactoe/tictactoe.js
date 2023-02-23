68a6900f8be3fbd46e0d9c1dde9930438d368169
bancheta21 <ancheta.b@northeastern.edu>
2023-02-23 18:43:52 -0500

    tic tac toe bug fixes

--- projects/tictactoe/tictactoe.js
+++ projects/tictactoe/tictactoe.js
@@ -21,6 +21,31 @@ for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", handleClick);
 }
 
+document.getElementById("play-again").addEventListener("click", handlePlayAgainClick);
+function handlePlayAgainClick() {
+    document.getElementById("1").innerHTML = "";
+    document.getElementById("2").innerHTML = "";
+    document.getElementById("3").innerHTML = "";
+    document.getElementById("4").innerHTML = "";
+    document.getElementById("5").innerHTML = "";
+    document.getElementById("6").innerHTML = "";
+    document.getElementById("7").innerHTML = "";
+    document.getElementById("8").innerHTML = "";
+    document.getElementById("9").innerHTML = "";
+    document.getElementById("1").disabled = false;
+    document.getElementById("2").disabled = false;
+    document.getElementById("3").disabled = false;
+    document.getElementById("4").disabled = false;
+    document.getElementById("5").disabled = false;
+    document.getElementById("6").disabled = false;
+    document.getElementById("7").disabled = false;
+    document.getElementById("8").disabled = false;
+    document.getElementById("9").disabled = false;
+    document.getElementById("play-again").style.opacity = 0;
+    document.getElementById("winner").innerHTML = "Tic Tac Toe";
+
+}
+
 function isGameOver() {
     if ((document.getElementById("1").innerHTML === "X"
     & document.getElementById("2").innerHTML === "X"
@@ -47,6 +72,10 @@ function isGameOver() {
     & document.getElementById("6").innerHTML === "X"
     & document.getElementById("9").innerHTML === "X")) {
         document.getElementById("winner").innerHTML = "Winner: X!";
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+        }
+        document.getElementById("play-again").style.opacity = 1;
     } else if 
         ((document.getElementById("1").innerHTML === "O"
     & document.getElementById("2").innerHTML === "O"
@@ -73,6 +102,9 @@ function isGameOver() {
     & document.getElementById("6").innerHTML === "O"
     & document.getElementById("9").innerHTML === "O")) {
         document.getElementById("winner").innerHTML = "Winner: O!";
-    
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+        }
+        document.getElementById("play-again").style.opacity = 1;
     }
 }
